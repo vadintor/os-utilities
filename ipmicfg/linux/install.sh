81417ecec6b2687c7800da22e38d90c7361ae170
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-14 10:40:59 +0100

    Fix folder names

--- ipmicfg/linux/install.sh
+++ ipmicfg/linux/install.sh
@@ -2,11 +2,12 @@
 # 1. Download ipmicfg from Supermicro from https://www.supermicro.com/SwDownload/SwSelect_Free.aspx?cat=IPMI
 downloadDir=~/Download
 ipmicfgDir=$downloadDir/IPMICFG
-mv IPMICFG*.zip IPMICFG.zip
-unzip IPMICFG.zip
-mv IPMICFG_* $IPMICFG 
-sudo cp $ ipmicfgDir/Linux/64bit/IPMICFG-Linux.x86_64 /usr/local/bin/ipmicfg
-sudo chmod 744 /usr/local/bin/ipmicfg
+binDir=/usr/local/bin/
+mv $downloadDir/IPMICFG*.zip  $downloadDir/IPMICFG.zip
+unzip $downloadDir/IPMICFG.zip -d $downloadDir
+mv $downloadDir/IPMICFG_* $ipmicfgDir
+sudo cp $ipmicfgDir/Linux/64bit/IPMICFG-Linux.x86_64 $binDir/ipmicfg
+sudo chmod 744 $binDir/ipmicfg
 sudo cp ./fanspeed.service /etc/systemd/system/ 
 sudo chmod 664 /etc/systemd/system//fanspeed.service
 sudo systemctl daemon-reload
